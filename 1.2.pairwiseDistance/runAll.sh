5cdfe756fbc606cb43fbc8841e6aece4a9fad7cb
nbedelman <edelmannate@gmail.com>
2021-03-22 07:53:08 -0400

    merged popstats

---
+++ 1.2.pairwiseDistance/runAll.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir -p data
+mkdir -p errs
+mkdir -p outs
+mkdir -p geno
+mkdir -p results
+
+ln -s /gpfs/loomis/project/skelly/aa2258/2019_RASY_YMF_PopGen/Analysis/old_ipy_analysis/YMF_main/popfile.txt data
+ln -s /home/nbe4/scratch60/1.3.pca/plink/RASY_ragtag/autosomes/data/RASY_ragtag.myers.autosome.filter.vcf data
+
+module load miniconda
+
+conda activate ipyrad
+###DEFINE VARIABLES###
+genomeDir=/home/nbe4/project/software/genomics_general/
+popfile=data/popfile.txt
+vcfFile=data/RASY_ragtag.myers.autosome.filter.vcf
+genofile=geno/RASY_ragtag.myers.autosome.filter.geno
+popgenStats=results/RASY_ragtag.myers.autosome.filter.vcf.stats
+
+###RUN CODE###
+#first, convert vcf to geno
+python $genomeDir/VCF_processing/parseVCF.py -i $vcfFile -o $genofile
+
+#then, get popgen stats
+sbatch code/computePopStats.slurm $genomeDir $genofile $popfile $popgenStats
