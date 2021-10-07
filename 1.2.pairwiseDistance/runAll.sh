ead9502c14fb5e2ffbaec60f9b60a620c023f7e2
Nate Edelman <nbedelman@users.noreply.github.com>
2021-10-07 11:36:57 -0400

    merge runAll

--- 1.2.pairwiseDistance/runAll.sh
+++ 1.2.pairwiseDistance/runAll.sh
@@ -7,17 +7,13 @@ mkdir -p geno
 mkdir -p results
 
 ln -s /gpfs/loomis/project/skelly/aa2258/2019_RASY_YMF_PopGen/Analysis/old_ipy_analysis/YMF_main/popfile.txt data
-<<<<<<< HEAD
 ln -s /home/nbe4/scratch60/1.3.pca/plink/RASY_ragtag/autosomes/data/RASY_ragtag.myers.autosome.filter.vcf data
-=======
 ln -s /gpfs/loomis/project/skelly/aa2258/2019_RASY_YMF_PopGen/filtered_vcfs/filter_SNPs/YMFmain.subset.0.50_0.50.vcf data
->>>>>>> c88abc52caf3eb0f608fcc3a7649363886806337
 
 module load miniconda
 
 conda activate ipyrad
 ###DEFINE VARIABLES###
-<<<<<<< HEAD
 genomeDir=/home/nbe4/project/software/genomics_general/
 popfile=data/popfile.txt
 vcfFile=data/RASY_ragtag.myers.autosome.filter.vcf
@@ -27,7 +23,6 @@ popgenStats=results/RASY_ragtag.myers.autosome.filter.vcf.stats
 ###RUN CODE###
 #first, convert vcf to geno
 python $genomeDir/VCF_processing/parseVCF.py -i $vcfFile -o $genofile
-=======
 genomeDir=/home/nbe4//project/software/genomics_general/
 popfile=data/popfile.txt
 vcfFile=data/YMFmain.subset.0.50_0.50.vcf
@@ -36,8 +31,7 @@ popgenStats=results/YMFmain.subset.0.50_0.50.stats
 
 ###RUN CODE###
 #first, convert vcf to geno
-# python $genomeDir/VCF_processing/parseVCF.py -i $vcfFile -o $genofile
->>>>>>> c88abc52caf3eb0f608fcc3a7649363886806337
+python $genomeDir/VCF_processing/parseVCF.py -i $vcfFile -o $genofile
 
 #then, get popgen stats
 sbatch code/computePopStats.slurm $genomeDir $genofile $popfile $popgenStats
